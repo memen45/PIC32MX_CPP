0019b19f8538416c27dd8f88e134db56cd5098ff
owner <owner@HP-ProDesk-600-G1-NEON>
2017-12-03 14:09:54 -0600

    initial commit of test code for pic32mm

--- newmain.cpp
+++ newmain.cpp
@@ -14,6 +14,8 @@ Pins leds[4] = {
 };
 Pins led2( Pins::PORT::C, 13 ); //LED2 (cp0 irq blinks)
 Pins sw1( Pins::PORT::B, 9 );   //SW1 (rotate delays))
+Pins sw2( Pins::PORT::C, 10 );  //SW2 cp0 irq blink rate++
+Pins sw3( Pins::PORT::C, 4 );   //SW3 cp0 irq blink rate--
 
 DelayCP0 sw_dly;                //debounce
 DelayCP0 dly[4];                //CP0 led delays
@@ -28,6 +30,8 @@ int main()
 {
     //init pins
     sw1.digital(); sw1.pullup_on(); sw1.lowison();
+    sw2.digital(); sw2.pullup_on(); sw2.lowison();
+    sw3.digital(); sw3.pullup_on(); sw3.lowison();
     led2.digital(); led2.output();
     for( auto& led : leds ){
         led.output();
@@ -36,7 +40,7 @@ int main()
 
     //cp0 irq, set sysfreq, compare timeout, enable irq + global irqs
     Cp0::sysfreq = 24;
-    Cp0::compare_ms( 250 );
+    Cp0::compare_ms( 200 );
     irq_cp0.enable();
     Irq::enable_all();
 
@@ -52,6 +56,15 @@ int main()
     //initialize delays
     rotate_delays();
 
+    auto irq_blinkrate = [ & ]( int16_t n ){
+        static uint16_t rate = 200;
+        rate += n;
+        if( rate < 100 || rate > 4000 ) rate -= n;
+        Irq::disable_all();
+        Cp0::compare_ms( rate );
+        Irq::enable_all();
+    };
+
     for (;;){
         for( auto i = 0; i < 4; i++ ){
             if( ! dly[i].isexpired() ) continue;
@@ -66,6 +79,18 @@ int main()
             //sw_dly sets while pressed
             //so will require sw release of 100ms
             //before timer can expire
+        }
+        if( sw2.ison() ){
+            if( sw_dly.isexpired() ){
+                irq_blinkrate( 100 ); //++
+            }
+            sw_dly.set_ms( 100 );       //sw debounce (use for both switches)
+        }
+        if( sw3.ison() ){
+            if( sw_dly.isexpired() ){
+                irq_blinkrate( -100 ); //--
+            }
+            sw_dly.set_ms( 100 );       //sw debounce (use for both switches)
         }
     }
 }
