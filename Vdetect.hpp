310234a2225a43d274b6cb12a73eb21add220ac6
owner <owner@HP-ProDesk-600-G1-NEON>
2017-12-03 21:58:51 -0600

    initial commit of test code for pic32mm

---
+++ Vdetect.hpp
@@ -0,0 +1,42 @@
+#ifndef _VDETECT_H
+#define _VDETECT_H
+
+#include <cstdint>
+
+namespace Vdetect {
+
+    enum {
+        HLVDCON = 0xBF802920,
+        HLVDCONCLR = 0xBF802924,
+        HLVDCONSET = 0xBF802928,
+        ON = 1<<15, SIDL = 1<<13, VDIR = 1<<11,
+        BGVST = 1<<10, IRVST = 1<<9,
+        HLEVT = 1<<8
+    };
+    enum HLVDL {
+        HLVDL_EXT = 15, HLVDL_200_222 = 14, HLVDL_208_233 = 13,
+        HLVDL_215_244 = 12, HLVDL_225_255 = 11, HLVDL_235_269 = 10,
+        HLVDL_245_280 = 9, HLVDL_265_298 = 8, HLVDL_275_309 = 7,
+        HLVDL_295_330 = 6, HLVDL_325_363 = 5
+    };
+
+    void on( void ){            *(volatile uint32_t*)(HLVDCONSET) = ON; }
+    void off( void ){           *(volatile uint32_t*)(HLVDCONCLR) = ON; }
+    
+    void stop_idle( void ){     *(volatile uint32_t*)(HLVDCONSET) = SIDL; }
+    void cont_idle( void ){     *(volatile uint32_t*)(HLVDCONCLR) = SIDL; }   
+
+    void trip_above( void ){    *(volatile uint32_t*)(HLVDCONSET) = VDIR; }
+    void trip_below( void ){    *(volatile uint32_t*)(HLVDCONCLR) = VDIR; } 
+    
+    bool bgap_stable( void ){   return *(volatile uint32_t*)(HLVDCON) & BGVST; }
+    bool iref_stable( void ){   return *(volatile uint32_t*)(HLVDCON) & IRVST; }
+    bool tripped( void ){       return *(volatile uint32_t*)(HLVDCON) & HLEVT; }
+    
+    void limit_set( HLVDL lvl ){
+        *(volatile uint32_t*)(HLVDCONCLR) = HLVDL_EXT;
+        *(volatile uint32_t*)(HLVDCONSET) = lvl;
+    }
+}
+
+#endif //_VDETECT_H
