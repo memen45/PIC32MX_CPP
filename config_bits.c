d9a6b5698c84a011e23608db27300af7ef1b961a
owner <owner@HP-ProDesk-600-G1-NEON>
2018-01-03 20:49:17 -0600

    initial commit of test code for pic32mm

--- config_bits.c
+++ config_bits.c
@@ -28,8 +28,8 @@
 #pragma config FWDTEN = OFF             // Watchdog Timer Enable bit (WDT is disabled)
 
 // FOSCSEL
-//#pragma config FNOSC = FRCDIV           // Oscillator Selection bits (Fast RC oscillator (FRC) with divide-by-N)
-#pragma config FNOSC = PLL              // Oscillator Selection bits (Primary or FRC oscillator with PLL)
+#pragma config FNOSC = FRCDIV           // Oscillator Selection bits (Fast RC oscillator (FRC) with divide-by-N)
+//#pragma config FNOSC = PLL              // Oscillator Selection bits (Primary or FRC oscillator with PLL)
 #pragma config PLLSRC = FRC             // System PLL Input Clock Selection bit (FRC oscillator is selected as PLL reference input on device reset)
 #pragma config SOSCEN = ON              // Secondary Oscillator Enable bit (Secondary oscillator (SOSC) is enabled)
 #pragma config IESO = OFF               // Two Speed Startup Enable bit (Two speed startup is disabled)
