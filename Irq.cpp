103e0a8ff8e358f6822f4245f920ae8fbd9da6c9
memen45 <memen45@gmail.com>
2018-02-26 22:19:41 +0100

    Fixed Irq class to support PIC32MX795F512L

--- Irq.cpp
+++ Irq.cpp
@@ -2,19 +2,65 @@
 #include "Reg.hpp"
 
 enum {
-    INTCON = 0xBF80F000,
+    INTCON = 0xBF881000,
         TPC_SHIFT = 8, TPC_CLR = 7,
         INT4EP = 4,
         INT3EP = 3,
         INT2EP = 2,
         INT1EP = 1,
         INT0EP = 0,
-    PRISS = 0xBF80F010,
-    INTSTAT = 0xBF80F020,
-    IFS_BASE = 0xBF80F040,
-    IEC_BASE = 0xBF80F0C0,
-    IPC_BASE = 0xBF80F140
+    INTSTAT = 0xBF881010,
+    IFS_BASE = 0xBF881030,
+    IEC_BASE = 0xBF881060,
+    IPC_BASE = 0xBF881090
 };
+
+const uint8_t Irq::m_lookup_vn[] = {
+        /*CORE_TIMER = */ 0, /* CORE_SOFTWARE_0 = */ 1, /* CORE_SOFTWARE_1 = */ 2,
+		
+		/* EXTERNAL_0 = */ 3, /* TIMER_1 = */ 4, 	/* INPUT_CAPTURE_1 =  */5, /* OUTPUT_COMPARE_1 = */ 6,
+		/* EXTERNAL_1 = */ 7, 	/* TIMER_2 = */ 8, 		/* INPUT_CAPTURE_2 = */ 9, 		/* OUTPUT_COMPARE_2 = */ 10,
+		/* EXTERNAL_2 = */ 11, 	/* TIMER_3 = */ 12, 		/* INPUT_CAPTURE_3 = */ 13, 		/* OUTPUT_COMPARE_3 = */ 14,
+		/* EXTERNAL_3 = */ 15, 	/* TIMER_4 = */ 16, 		/* INPUT_CAPTURE_4 = */ 17, 		/* OUTPUT_COMPARE_4 = */ 18,
+		/* EXTERNAL_4 = */ 19, 	/* TIMER_5 = */ 20, 		/* INPUT_CAPTURE_5 = */ 21, 		/* OUTPUT_COMPARE_5 = */ 22,
+		
+		/* SPI1_ERR = */ 23, /* SPI1_RX = */ 23, /* SPI1_TX = */ 23,
+		
+		/* UART1_ERR = */ 24, /* SPI3_ERR = */ 24, /* I2C3_BUS = */ 24,
+		/* UART1_RX = */ 24, /* SPI3_RX = */ 24, /* I2C3_SLAVE = */ 24,
+		/* UART1_TX = */ 24, /* SPI3_TX = */ 24, /* I2C3_MASTER = */ 24,
+		
+		/* I2C1_BUS = */ 25, /* I2C1_SLAVE = */ 25, /* I2C1_MASTER = */ 25,
+		/* CHANGE_NOTICE = */ 26,
+		/* ADC = */ 27,
+		/* PMP = */ 28,
+		/* CMP_1 = */ 29, /* CMP_2 = */ 30,
+		
+		/* UART3_ERR = */ 31, /* SPI2_ERR = */ 31, /* I2C4_BUS = */ 31,
+		/* UART3_RX = */ 31, /* SPI2_RX = */ 31, /* I2C4_SLAVE = */ 31,
+		/* UART3_TX = */ 31, /* SPI2_TX = */ 31, /* I2C4_MASTER = */ 31,
+		
+		/* UART2_ERR = */ 32, /* SPI4_ERR = */ 32, /* I2C5_BUS = */ 32,
+		/* UART2_RX = */ 32,/*  SPI4_RX = */ 32, /* I2C5_SLAVE = */ 32,
+		/* UART2_TX = */ 32, /* SPI4_TX = */ 32, /* I2C5_MASTER = */ 32,
+		
+		/* I2C2_BUS = */ 33, /* I2C2_SLAVE = */ 33, /* I2C2_MASTER = */ 33,
+		
+		/* FSCM = */ 34,
+		/* RTCC = */ 35,
+		/* DMA0 = */ 36, /* DMA1 = */ 37, /* DMA2 = */ 38, /* DMA3 = */ 39, /* DMA4 = */ 40, /* DMA5 = */ 41, /* DMA6 = */ 42, /* DMA7 = */ 43,
+		/* FCE = */ 44,
+		/* USB = */ 45,
+		/* CAN1 = */ 46, /* CAN2 = */ 47,
+		/* ETHERNET = */ 48,
+		
+		/* INPUT_CAPTURE_1_ERROR = */ 5, /* INPUT_CAPTURE_2_ERROR = */ 9, /* INPUT_CAPTURE_3_ERROR = */ 13, /* INPUT_CAPTURE_4_ERROR = */ 17, /* INPUT_CAPTURE_5_ERROR = */ 21,
+		/* PMP_ERROR = */ 28, 
+		
+		/* UART4_ERR = */ 49, /* UART4_RX = */ 49, /* UART4_TX = */ 49,
+		/* UART6_ERR = */ 50, /* UART6_RX = */ 50, /* UART6_TX = */ 50,	
+		/* UART5_ERR = */ 51, /* UART5_RX = */ 51, /* UART5_TX = */ 51
+    };
 
 //=============================================================================
     void        Irq::disable_all        ()
@@ -84,47 +130,48 @@ enum {
 //numbers are enums (not uint32_t*)- so need to calculate bytes
 //the Reg::set/clr will reinterpret the enums to volatile uint32_t*
 //
-//vector 40 example
-//reg = 0xBF80F040 + (40/32)*16 = 0xBF80F050 = IFS1
-//bit offset = 1<<(40%32) = 1<<8
-//bit offset 8 in IFS1
+//vector 40 example												IRQ number: 66
+//reg = 0xBF80F040 + (40/32)*16 = 0xBF80F050 = IFS1				+(66/32)*16 = +32
+//bit offset = 1<<(40%32) = 1<<8								1<<(66%32) = 1<<2
+//bit offset 8 in IFS1											offset 2 in IFS2
 //=============================================================================
-    void        Irq::flag_clr           (IRQ_VN e)
+    void        Irq::flag_clr           (IRQ_NR e)
 //=============================================================================
 {
     Reg::clrbit(IFS_BASE + ((e / 32) * 16), 1u<<(e % 32));
 }
 
 //=============================================================================
-    bool        Irq::flag               (IRQ_VN e)
+    bool        Irq::flag               (IRQ_NR e)
 //=============================================================================
 {
     return Reg::anybit(IFS_BASE + ((e / 32) * 16), 1u<<(e % 32));
 }
 
 //=============================================================================
-    void        Irq::on                 (IRQ_VN e, bool tf)
+    void        Irq::on                 (IRQ_NR e, bool tf)
 //=============================================================================
 {
     Reg::setbit(IEC_BASE + ((e / 32) * 16), 1u<<(e % 32), tf);
 }
 
-//vector 17 example
-//priority_shift = 8*(17%4) =  8*1= 8
-//reg = 0xBF80F140 + (17/4)*16 = 0xBF80F180 = IPC4
-//priority bits at bit offset 8 in IPC4
+//vector 17 example												Vector number: 28
+//priority_shift = 8*(17%4) =  8*1= 8							8*(28%4) = 8*2 = 0
+//reg = 0xBF80F140 + (17/4)*16 = 0xBF80F180 = IPC4				+ (28/4)*16 = +112
+//priority bits at bit offset 8 in IPC4							offset 0 in IPC7
 //
 //init specific irq
 //pri is masked to 0-7, sub is masked to 0-3
 //pri of 0 disables irq
 //=============================================================================
-    void        Irq::init       (IRQ_VN e, uint8_t pri, uint8_t sub, bool tf)
+    void        Irq::init       (IRQ_NR e, uint8_t pri, uint8_t sub, bool tf)
 //=============================================================================
 {
-    uint32_t priority_shift = 8 * (e % 4);
+	uint8_t vn = m_lookup_vn[e];
+    uint32_t priority_shift = 8 * (vn % 4);
     pri and_eq 7; sub and_eq 3; pri <<= 2; pri or_eq sub;
-    Reg::clrbit(IPC_BASE + ((e / 4) * 16), (31<<priority_shift));
-    Reg::setbit(IPC_BASE + ((e / 4) * 16), (pri<<priority_shift));
+    Reg::clrbit(IPC_BASE + ((vn / 4) * 16), (31<<priority_shift));
+    Reg::setbit(IPC_BASE + ((vn / 4) * 16), (pri<<priority_shift));
     flag_clr(e);
     on(e, tf);
 }
@@ -137,18 +184,6 @@ enum {
     for(; arr->irqvn not_eq END; arr++){
         init(arr->irqvn, arr->p, arr->s, arr->en);
     }
-}
-
-//priority shadow set, 0 or 1
-//pri = 1-7, tf =0,1 (0=normal, 1=shadow)
-//pri is masked to 0-7
-//pri val of 0 will set/clr SS0- no harm as not using (single vector ss)
-//=============================================================================
-    void        Irq::shadow_set         (uint8_t pri, bool tf)
-//=============================================================================
-{
-    pri and_eq 7; pri <<= 2; //0*4=0 1*4=4, 7*4=28
-    Reg::setbit(PRISS, 1<<pri, tf);
 }
 
 
--- Irq.hpp
+++ Irq.hpp
@@ -18,65 +18,70 @@ struct Irq {
     static void     eint1_pol       (EINTXPOL);
     static void     eint0_pol       (EINTXPOL);
 
-    //irq vector numbers
-    enum IRQ_VN : uint8_t {
+	//irq numbers
+    enum IRQ_NR : uint8_t {
         CORE_TIMER = 0, CORE_SOFTWARE_0, CORE_SOFTWARE_1,
-        EXTERNAL_0 = 3, EXTERNAL_1, EXTERNAL_2, EXTERNAL_3, EXTERNAL_4,
-        CHANGE_NOTICE_A = 8, CHANGE_NOTICE_B, CHANGE_NOTICE_C, CHANGE_NOTICE_D,
-        //12,13,14,15,16
-        TIMER_1 = 17, TIMER_2, TIMER_3,
-        //20,21,23
-        COMPARATOR_1 = 23, COMPARATOR_2, COMPARATOR_3,
-        //26,27,28
-        USB = 29,
-        RTCC = 32,
-        ADC = 33,
-        HLVD = 36,
-        CLC1 = 37, CLC2, CLC3, CLC4,
-        SPI1_ERR = 41, SPI1_TX, SPI1_RX,
-        SPI2_ERR = 44, SPI2_TX, SPI2_RX,
-        SPI3_ERR = 47, SPI3_TX, SPI3_RX,
-        //50,51,52
-        UART1_RX = 53, UART1_TX, UART1_ERR,
-        UART2_RX = 56, UART2_TX, UART2_ERR,
-        UART3_RX = 59, UART3_TX, UART3_ERR,
-        //62,63,64
-        I2C1_SLAVE = 65, I2C1_MASTER, I2C1_BUS,
-        I2C2_SLAVE = 68, I2C2_MASTER, I2C2_BUS,
-        I2C3_SLAVE = 71, I2C3_MASTER, I2C3_BUS,
-        CCP1 = 74, CCT1,
-        CCP2 = 76, CCT2,
-        CCP3 = 78, CCT3,
-        CCP4 = 80, CCT4,
-        CCP5 = 82, CCT5,
-        CCP6 = 84, CCT6,
-        CCP7 = 86, CCT7,
-        CCP8 = 88, CCT8,
-        CCP9 = 90, CCT9,
-        FRC_TUNE = 92,
-        //93
-        NVM = 94,
-        PERFORMANCE_COUNTER = 95,
-        //96
-        ECCSB_ERR = 97,
-        DMA0 = 98, DMA1, DMA2, DMA3 = 101,
-        END = 255
+		
+		EXTERNAL_0 = 3, TIMER_1 = 4, 	INPUT_CAPTURE_1 = 5, 	OUTPUT_COMPARE_1 = 6,
+		EXTERNAL_1, 	TIMER_2, 		INPUT_CAPTURE_2, 		OUTPUT_COMPARE_2,
+		EXTERNAL_2, 	TIMER_3, 		INPUT_CAPTURE_3, 		OUTPUT_COMPARE_3,
+		EXTERNAL_3, 	TIMER_4, 		INPUT_CAPTURE_4, 		OUTPUT_COMPARE_4,
+		EXTERNAL_4, 	TIMER_5, 		INPUT_CAPTURE_5, 		OUTPUT_COMPARE_5,
+		
+		SPI1_ERR = 23, SPI1_RX = 24, SPI1_TX = 25,
+		
+		UART1_ERR = 26, SPI3_ERR = 26, I2C3_BUS = 26,
+		UART1_RX = 27, SPI3_RX = 27, I2C3_SLAVE = 27,
+		UART1_TX = 28, SPI3_TX = 28, I2C3_MASTER = 28,
+		
+		I2C1_BUS = 29, I2C1_SLAVE, I2C1_MASTER,
+		CHANGE_NOTICE = 32,
+		ADC = 33,
+		PMP = 34,
+		CMP_1 = 35, CMP_2,
+		
+		UART3_ERR = 37, SPI2_ERR = 37, I2C4_BUS = 37,
+		UART3_RX = 38, SPI2_RX = 38, I2C4_SLAVE = 38,
+		UART3_TX = 39, SPI2_TX = 39, I2C4_MASTER = 39,
+		
+		UART2_ERR = 40, SPI4_ERR = 40, I2C5_BUS = 40,
+		UART2_RX = 41, SPI4_RX = 41, I2C5_SLAVE = 41,
+		UART2_TX = 42, SPI4_TX = 42, I2C5_MASTER = 42,
+		
+		I2C2_BUS = 43, I2C2_SLAVE, I2C2_MASTER,
+		
+		FSCM = 46,
+		RTCC = 47,
+		DMA0 = 48, DMA1, DMA2, DMA3, DMA4, DMA5, DMA6, DMA7,
+		FCE = 56,
+		USB = 57,
+		CAN1 = 58, CAN2,
+		ETHERNET = 60,
+		
+		INPUT_CAPTURE_1_ERROR = 61, INPUT_CAPTURE_2_ERROR, INPUT_CAPTURE_3_ERROR, INPUT_CAPTURE_4_ERROR, INPUT_CAPTURE_5_ERROR,
+		PMP_ERROR = 66, 
+		
+		UART4_ERR = 67, UART4_RX, UART4_TX,
+		UART6_ERR = 70, UART6_RX, UART6_TX,	
+		UART5_ERR = 73, UART5_RX, UART5_TX,
+		END = 255
     };
-    static void     flag_clr        (IRQ_VN);
-    static bool     flag            (IRQ_VN);
-    static void     on              (IRQ_VN, bool);
-    static void     init            (IRQ_VN, uint8_t, uint8_t, bool);
+
+    static void     flag_clr        (IRQ_NR);
+    static bool     flag            (IRQ_NR);
+    static void     on              (IRQ_NR, bool);
+    static void     init            (IRQ_NR, uint8_t, uint8_t, bool);
 
     //to create a list (array) of irq's to init/enable
     using irq_list_t = struct {
-        IRQ_VN irqvn;   //vector number
+        IRQ_NR irqvn;   //vector number
         uint8_t p;      //priority
         uint8_t s;      //sub-priority
         bool en;        //enable
     };
     static void     init            (irq_list_t*);
-
-    static void     shadow_set      (uint8_t, bool);
+	
+	static const uint8_t m_lookup_vn[94];
 
 };
 
