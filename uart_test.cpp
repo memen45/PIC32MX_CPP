9cace9e3f2fddded94c6abad5a52eaac4c1b80b4
cv007 <32439630+cv007@users.noreply.github.com>
2018-08-05 12:18:59 -0500

    Update uart_test.cpp

--- uart_test.cpp
+++ uart_test.cpp
@@ -5,13 +5,15 @@ int main()
 {
     //set osc to 24MHz
     Osc osc;
-    osc.pll_set(osc.MUL12, osc.DIV4);
+    osc.pll_set ( osc.MUL12, osc.DIV4 );
     //tx only
-    Uart info{Uart::UART2TX, Pins::C6, 230400};
-    info.on(true);
+    Uart info { Uart::UART2TX, Pins::C6, 230400 };
+    //8N1 (is default, so really not needed to be set)
+    info.mode ( info.MODE8N1 );
+    info.on ( true );
 
     for(;;){ 
-        info.puts("Hello World? ");
+        info.puts ( "Hello World? " );
     }
 }
 
