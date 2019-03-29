afe417d37edd25f2a9ba29e830913f5db7993850
cv007 <curtvm@msn.com>
2019-03-28 21:29:51 -0500

    initial commit of test code for pic32mm

--- stepper_test.cpp
+++ stepper_test.cpp
@@ -21,10 +21,10 @@
 #include "Uart.hpp"
 
 Uart info{Uart::UART2, Pins::C6, Pins::C7, 1000000};
-Pins sw3{ Pins::C4, Pins::INPU }; //enable/disable stepper test
-Pins sw2{ Pins::C10, Pins::INPU }; //brake enable/disable
-Pins pot{ Pins::AN14 }; //stepper speed, check pot val via adc
-Pins stepper_pins[4]{
+Pins sw3{ Pins::C4, Pins::INPU };   //enable/disable stepper test
+Pins sw2{ Pins::C10, Pins::INPU };  //brake enable/disable
+Pins pot{ Pins::AN14 };             //set stepper speed
+Pins stepper_pins[4]{               //stepper pins
     {Pins::C0, Pins::OUT}, //AI1
     {Pins::C2, Pins::OUT}, //AI2
     {Pins::B0, Pins::OUT}, //BI1
@@ -33,7 +33,10 @@ Pins stepper_pins[4]{
 
 struct StepperDriver {
 
-    StepperDriver( Pins (&pins)[4] ) : m_pins(pins) {}
+    StepperDriver( Pins (&pins)[4], uint16_t steps )
+    : m_pins(pins), m_steps_rev(steps)
+    {
+    }
 
     void stop(void){
         for( auto& p : m_pins ) p.off();
@@ -47,16 +50,20 @@ struct StepperDriver {
         m_pins[1].on();
     }
 
-    void step(int n, uint32_t dly = 5_ms){
+    void step(int n, uint32_t rpm = 150){
         static uint8_t step_idx;
         if( n == 0 ) return;
         int dir = n > 0 ? 1 : -1;
         if( n < 0 ) n = -n;
-        if( dly < 2_ms ) dly = 2_ms;
+        //if( dly < 2_ms ) dly = 2_ms;
+        if( rpm > 150 ) rpm = 150;
+        //dly in us calculated from rpm
+        uint32_t dly = 60000000 / (rpm * m_steps_rev);
         while(n--){
-            for(auto i = 0; i < 4; i++){
-                m_pins[i].latval( m_step_table[step_idx][i] );
-            }
+            m_pins[0].latval( m_step_table[step_idx][0] ); //AI1
+            m_pins[1].latval( !m_step_table[step_idx][0] ); //AI2
+            m_pins[2].latval( m_step_table[step_idx][1] ); //BI1
+            m_pins[3].latval( !m_step_table[step_idx][1] ); //BI2
             step_idx += dir;
             step_idx and_eq 3;
             Delay::wait( dly );
@@ -66,10 +73,11 @@ struct StepperDriver {
 
     private:
 
-    Pins (&m_pins)[4];
-
-    const bool m_step_table[4][4]{
-        {1,0,0,1}, {0,1,0,1}, {0,1,1,0}, {1,0,1,0}
+    Pins (&m_pins)[4]; //AI1, AI2, BI1,BI2
+    uint16_t m_steps_rev; //steps per rev
+    const bool m_step_table[4][2]{
+        //AI1 (AI2=!AI1), BI1 (BI2=!BI1)
+        {1,0}, {0,0}, {0,1}, {1,1}
     };
 
 };
@@ -91,10 +99,10 @@ int main()
     //! reset colors/attributes/cls/home
     info.printf("{+!Y}\r\nTesting {/M}Stepper Motor driver{|W}\r\n");
 
-    StepperDriver s( stepper_pins );
+    StepperDriver s( stepper_pins, 200 );
     bool en = false, brake = false;;
-    int steps = 400;
-    uint32_t speed = 2_ms;
+    int steps = 400; //2 turns
+    uint32_t speed = 150; //rpm (fastest)
 
     auto check_sw = [&](){
         if( sw3.ison() ){
@@ -118,11 +126,11 @@ int main()
     //pot controls step speed 2000us-6095us
     for(;;){
         check_sw();
-        //adc vals 0-4095 -> speed range 2_ms to 2_ms+(4095us)
-        //speed = 2000us to 6095us
-        speed = pot.adcval() + 2_ms;
+        //adc vals 0-4095 -> 0-127
+        //speed range 150rpm - 23rpm
+        speed = (pot.adcval()>>5) + 23;
         if( en ){
-            info.printf( "step: {G}%+d{W}  speed: {G}%d us{W}\r\n", steps, speed );
+            info.printf( "step: {G}%+d{W}  speed: {G}%d rpm{W}\r\n", steps, speed );
             int i = steps > 0 ? steps : -steps;
             for(; en and i; i--, check_sw()){
                 s.step( steps > 0 ? 1 : -1, speed );
